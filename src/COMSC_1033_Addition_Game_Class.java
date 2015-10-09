07a86a944c039fbb368784bc1be4516b8c436b21
Yuanyuan Yang <yangy2@student.swosu.edu>
2015-10-09 14:39:18 -0500

    Able to run a basic test case.

--- src/COMSC_1033_Addition_Game_Class.java
+++ src/COMSC_1033_Addition_Game_Class.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class COMSC_1033_Addition_Game_Class {
 	public static void main(String[] args) {
 		System.out.println("Hello Class!");
@@ -8,18 +9,36 @@ public class COMSC_1033_Addition_Game_Class {
 		//	Generate 2 random numbers
 		int number1 = (int)(Math.random()*10);
 		System.out.println("Random number 1 is: " + number1);
+		int number2 = (int)(Math.random()*10);
+		System.out.println("Random number 2 is: " + number2);
+		
+		int correctAnswer = number1 + number2;
 		//  Ask the user to add these two numbers together
+		System.out.println("What is " + number1 + " + " + number2 +" = ?");
+
 		//  Read in their response
+		Scanner input = new Scanner(System.in);
+		int studentAnswer = input.nextInt();
+
 		//  Check if the answer was correct
-		//		IF correct
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
-		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		//			Make the next question easier
+		if(correctAnswer == studentAnswer){
+			//		IF correct
+			//			Tell them it was correct
+			System.out.println("Your answer was correct");
+			//			Give them points
+			//			Make the next question harder
+			
+		}else{
+			//		IF not correct
+			//			Tell them it was wrong
+			System.out.println("Your answer was not correct");
+			// 			Tell them the correct answer
+			//			Do not give them points
+			//			Make the next question easier
+			
+		}
+
+
 		// Round 2
 		//	Generate 2 random numbers
 		//  Ask the user to add these two numbers together
