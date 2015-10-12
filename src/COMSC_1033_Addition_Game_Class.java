ad2949d27a02b3063738e9fa86d4d47283d7129c
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-12 07:20:33 -0500

    Tidy up the code and comments and test for 2 working conditions.

--- src/COMSC_1033_Addition_Game_Class.java
+++ src/COMSC_1033_Addition_Game_Class.java
@@ -25,20 +25,19 @@ public class COMSC_1033_Addition_Game_Class {
 		//  Check if the answer was correct
 		//		IF correct
 		if (studentAnswer == correctAnswer){
+			//			Tell them it was correct
+			//			Give them points
+			//			Make the next question harder
 			System.out.println("Answer was correct");
-		}else{
-			System.out.println("Answer was not correct");
 		}
-			
-		
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
 		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		//			Make the next question easier
+		else{
+			//			Tell them it was wrong
+			System.out.println("Answer was not correct");
+			// 			Tell them the correct answer
+			//			Do not give them points
+			//			Make the next question easier
+		}
 		// Round 2
 		//	Generate 2 random numbers
 		//  Ask the user to add these two numbers together
