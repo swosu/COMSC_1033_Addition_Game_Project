975584c6686c182b7df5b385a86c42f06e91ffc6
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-12 09:53:13 -0500

    Working round 1 start

--- src/COMSC_1033_Addition_Game_Class.java
+++ src/COMSC_1033_Addition_Game_Class.java
@@ -5,14 +5,18 @@ public class COMSC_1033_Addition_Game_Class {
 		// This is my master branch.
 		// Outline for addition problem.
 		
+		int score = 0;
+		int hardnessOfQuestion = 1000;
+		
 		// Round 1
 		//	Generate 2 random numbers
-		int number1 = (int)(Math.random() *10);
+		int number1 = (int)(Math.random() * hardnessOfQuestion);
 		System.out.println("Number1 is :" + number1 );
 		
-		int number2 = (int)(Math.random() *10);
+		int number2 = (int)(Math.random() * hardnessOfQuestion);
 		System.out.println("Number2 is :" + number2 );
 		
+		// to keep track of the correct answer.
 		int correctAnswer = number1 + number2;
 		
 		//  Ask the user to add these two numbers together
@@ -26,15 +30,20 @@ public class COMSC_1033_Addition_Game_Class {
 		//		IF correct
 		if (studentAnswer == correctAnswer){
 			//			Tell them it was correct
-			//			Give them points
-			//			Make the next question harder
 			System.out.println("Answer was correct");
+			//			Give them points
+			score += hardnessOfQuestion;
+			System.out.println("Score is: " + score);
+			//			Make the next question harder
+			hardnessOfQuestion *= 10;	
+			System.out.println("hardness is: " + hardnessOfQuestion);
 		}
 		//		IF not correct
 		else{
 			//			Tell them it was wrong
 			System.out.println("Answer was not correct");
 			// 			Tell them the correct answer
+			System.out.println("The correct answer was: " + correctAnswer);
 			//			Do not give them points
 			//			Make the next question easier
 		}
