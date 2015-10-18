44baef075740c607e71a54d6f3ee047113232568
unknown <je640817@EvertJ-PC.admin.swosu.edu>
2015-10-18 16:16:40 -0500

    added all the working code from a different branch

--- src/COMSC_1033_Addition_Game_Class.java
+++ src/COMSC_1033_Addition_Game_Class.java
@@ -2,77 +2,193 @@ import java.util.Scanner;
 public class COMSC_1033_Addition_Game_Class {
 	public static void main(String[] args) {
 		System.out.println("Hello Class!");
-		// This is my master branch.
+		// This is my development 1412 branch.
+		
 		// Outline for addition problem.
+		int score = 0;
+		int hardness = 10;
 		
 		// Round 1
 		//	Generate 2 random numbers
-		//  Ask the user to add these two numbers together		
+		int number1 = (int)(Math.random() * hardness);
+		int number2 = (int)(Math.random() * hardness);
+		int correctAnswer = number1 + number2;
+		
+		//  Ask the user to add these two numbers together
+		System.out.println("What integer is " + number1 + " + " + number2 + "?");
+		System.out.println("Please enter integers only.");
 		//  Read in their response
-		//  Check if the answer was correct		
-			//		IF correct
-			//			Tell them it was correct			
-			//			Give them points
-			//			Make the next question harder		
-			//		IF not correct
-			//			Tell them it was wrong			
-			// 			Tell them the correct answer
-			//			Do not give them points
-			//			Make the next question easier
-		//  Ask the user to add these two numbers together		
-		//  Read in their response		
+		Scanner input = new Scanner(System.in);
+		int studentAnswer = input.nextInt();
 		//  Check if the answer was correct
-		//		IF correct		
+		if(studentAnswer == correctAnswer){
+			//		IF correct
 			//			Tell them it was correct
+			System.out.println("Your answer was correct.");
 			//			Give them points
-			//			Make the next question harder		
-		//		IF not correct		
+			score += hardness;
+			System.out.println("Your score is now: " + score);
+			//			Make the next question harder
+			hardness *= 10;
+			System.out.println("The next hardness will be: " + hardness);
+		}else{
+			//		IF not correct
 			//			Tell them it was wrong
+			System.out.println("Your answer was not correct.");
 			// 			Tell them the correct answer
+			System.out.println("The correct answer was: " + correctAnswer);
 			//			Do not give them points
+			score += 0;
 			//			Make the next question easier
+			if (hardness > 10){
+				hardness /= 10;
+				System.out.println("Your hardness is now: " + hardness);
+			}else{
+				System.out.println("Your hardness is at the lowest level");
+				System.out.println("Your hardness is now: " + hardness);
+			}
+			
+			
+		}
+
+		System.out.println("End of round 1.");
+		
 		// Round 2
 		//	Generate 2 random numbers
+		number1 = (int)(Math.random() * hardness);
+		number2 = (int)(Math.random() * hardness);
+		correctAnswer = number1 + number2;
+		
 		//  Ask the user to add these two numbers together
+		System.out.println("What integer is " + number1 + " + " + number2 + "?");
+		System.out.println("Please enter integers only.");
 		//  Read in their response
+		//Scanner input = new Scanner(System.in);
+		studentAnswer = input.nextInt();
 		//  Check if the answer was correct
-		//		IF correct
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
-		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		//			Make the next question easier
+		if(studentAnswer == correctAnswer){
+			//		IF correct
+			//			Tell them it was correct
+			System.out.println("Your answer was correct.");
+			//			Give them points
+			score += hardness;
+			System.out.println("Your score is now: " + score);
+			//			Make the next question harder
+			hardness *= 10;
+			System.out.println("The next hardness will be: " + hardness);
+		}else{
+			//		IF not correct
+			//			Tell them it was wrong
+			System.out.println("Your answer was not correct.");
+			// 			Tell them the correct answer
+			System.out.println("The correct answer was: " + correctAnswer);
+			//			Do not give them points
+			score += 0;
+			//			Make the next question easier
+			if (hardness > 10){
+				hardness /= 10;
+				System.out.println("Your hardness is now: " + hardness);
+			}else{
+				System.out.println("Your hardness is at the lowest level");
+				System.out.println("Your hardness is now: " + hardness);
+			}
+			
+			
+		}
+
+		System.out.println("End of round 2.");
+		
 		// Round 3
-		//	Generate 2 random numbers
+//		Generate 2 random numbers
+		number1 = (int)(Math.random() * hardness);
+		number2 = (int)(Math.random() * hardness);
+		correctAnswer = number1 + number2;
+		
 		//  Ask the user to add these two numbers together
+		System.out.println("What integer is " + number1 + " + " + number2 + "?");
+		System.out.println("Please enter integers only.");
 		//  Read in their response
+		//Scanner input = new Scanner(System.in);
+		studentAnswer = input.nextInt();
 		//  Check if the answer was correct
-		//		IF correct
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
-		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		//			Make the next question easier
+		if(studentAnswer == correctAnswer){
+			//		IF correct
+			//			Tell them it was correct
+			System.out.println("Your answer was correct.");
+			//			Give them points
+			score += hardness;
+			System.out.println("Your score is now: " + score);
+			//			Make the next question harder
+			hardness *= 10;
+			System.out.println("The next hardness will be: " + hardness);
+		}else{
+			//		IF not correct
+			//			Tell them it was wrong
+			System.out.println("Your answer was not correct.");
+			// 			Tell them the correct answer
+			System.out.println("The correct answer was: " + correctAnswer);
+			//			Do not give them points
+			score += 0;
+			//			Make the next question easier
+			if (hardness > 10){
+				hardness /= 10;
+				System.out.println("Your hardness is now: " + hardness);
+			}else{
+				System.out.println("Your hardness is at the lowest level");
+				System.out.println("Your hardness is now: " + hardness);
+			}
+			
+			
+		}
+
+		System.out.println("End of round 3.");
 		// Round 4
-		//	Generate 2 random numbers
+//		Generate 2 random numbers
+		number1 = (int)(Math.random() * hardness);
+		number2 = (int)(Math.random() * hardness);
+		correctAnswer = number1 + number2;
+		
 		//  Ask the user to add these two numbers together
+		System.out.println("What integer is " + number1 + " + " + number2 + "?");
+		System.out.println("Please enter integers only.");
 		//  Read in their response
+		//Scanner input = new Scanner(System.in);
+		studentAnswer = input.nextInt();
 		//  Check if the answer was correct
-		//		IF correct
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
-		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		// 	Print final score.
-		//			Make the next question easier
+		if(studentAnswer == correctAnswer){
+			//		IF correct
+			//			Tell them it was correct
+			System.out.println("Your answer was correct.");
+			//			Give them points
+			score += hardness;
+			System.out.println("Your score is now: " + score);
+			//			Make the next question harder
+			hardness *= 10;
+			System.out.println("The next hardness will be: " + hardness);
+		}else{
+			//		IF not correct
+			//			Tell them it was wrong
+			System.out.println("Your answer was not correct.");
+			// 			Tell them the correct answer
+			System.out.println("The correct answer was: " + correctAnswer);
+			//			Do not give them points
+			score += 0;
+			//			Make the next question easier
+			if (hardness > 10){
+				hardness /= 10;
+				System.out.println("Your hardness is now: " + hardness);
+			}else{
+				System.out.println("Your hardness is at the lowest level");
+				System.out.println("Your hardness is now: " + hardness);
+			}
+			
+			
+		}
+
+		System.out.println("End of round 4.");
+		System.out.println("Your final score is: " + score);
+
+
+
 	}
 }
